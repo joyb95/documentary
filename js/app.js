8039b423e20235a3e142e3144f0b8c6426e8d762
joyb95 <bjoy64874@gmail.com>
2022-12-19 19:16:14 +0600

    chenged

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 
-
-// dark theme matching that system
-
+// for header sticky
+let headerMainWrap = document.querySelector('.headerMainWrap');
+window.addEventListener('scroll', function(){
+    headerMainWrap.classList.toggle('sticky', window.scrollY > 0);
+})
 
 // for menu icon toggle
 let menuIcon = document.querySelector('.menuIcon');
 
+let primaryMenuWrap = document.querySelector('.primaryMenuWrap');
+
 menuIcon.onclick = function (){
     menuIcon.classList.toggle('active');
+    primaryMenuWrap.classList.toggle('active');
 }
